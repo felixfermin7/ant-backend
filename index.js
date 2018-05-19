8104adfb282347e17911b8f0886718b20d6e9e4c
felix <felix@userlab.co>
2018-05-19 10:28:20 -0600

    ants crud

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
 const express = require('express')
 const app = express()
 const { Ants } = require('./lib/ants/src/')
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json()) // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/ants/:id', (req, res, next) =>
   Ants.get(req.params.id)
-    .then(ants => res.json(ants))
+    .then(ant => res.json(ant))
     .catch(next)
 //  return (res.send('Hello World!'))
 )
 
 app.get('/ants', (req, res, next) =>
   Ants.list()
+    .then(ants => res.json(ants))
+    .catch(next)
 )
 
-app.post('/ants', (req, res, next) => {
+app.post('/ants', (req, res, next) =>
   Ants.create(req.body.ant)
-  return (res.send('Hello World!'))
-})
+    .then(ant => res.json(ant))
+    .catch(next)
+)
 
-app.put('/ants/:id', (req, res, next) => {
+app.put('/ants/:id', (req, res, next) =>
   Ants.update(req.params.id, req.body.ant)
-  return (res.send('Hello World!'))
-})
+    .then(ant => res.json(ant))
+    .catch(next)
+)
 
-app.delete('/ants/:id', (req, res, next) => {
+app.delete('/ants/:id', (req, res, next) =>
   Ants.delete(req.params.id)
-  return (res.send('Hello World!'))
-})
+    .then(response => res.json(response))
+    .catch(next)
+)
 
 app.listen(4000, err => {
   if (err) return console.error(err)
